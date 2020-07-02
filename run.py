cfed78aaa8cc32408f9e11add4a2a074062d98aa
tracyjohnson213 <tracyjohnson213@gmail.com>
2020-07-02 05:58:46 +0000

    add navigation items

--- run.py
+++ run.py
@@ -9,6 +9,21 @@ def index():  # defined view
     return render_template('index.html')
 
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':  # default module of python
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
