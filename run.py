7b93fdcabb662cc70ab320b9635990bdac207df1
tracyjohnson213 <tracyjohnson213@gmail.com>
2020-07-05 20:34:46 +0000

    add contact form

--- run.py
+++ run.py
@@ -1,16 +1,19 @@
 import os
 import json
-from flask import Flask, render_template  # Flask class
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)  # instance of Flask
+app.secret_key = "some_secret"
 
 
-@app.route('/')  # route and view for home page
+# route and view for home page
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/services')  # route and view for services page
+# route and view for services page
+@app.route('/services')
 def services():
     data = []
     with open("data/services.json", "r") as json_data:
@@ -19,7 +22,8 @@ def services():
         'services.html', page_title='Services', services=data)
 
 
-@app.route('/services/<service_name>')  # route and view for specific service
+# route and view for specific service
+@app.route('/services/<service_name>')
 def about_service(service_name):
     service = {}
     with open("data/services.json", "r") as json_data:
@@ -30,17 +34,23 @@ def about_service(service_name):
     return render_template('service.html', service=service)
 
 
-@app.route('/contact')  # route and view for contact page
+# route and view for contact page
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".
+              format(request.form["name"]))
     return render_template('contact.html', page_title='Contact')
 
 
-@app.route('/login')  # route and view for login page
+# route and view for login page
+@app.route('/login')
 def login():
     return render_template('login.html', page_title='Login')
 
 
-if __name__ == '__main__':  # default module of python
+# default module of python
+if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
